8daa4ebd9845e25b037544ff983b8af4da8aba53
Demo <384588466@qq.com>
2018-10-15 09:18:55 +0800

    Init Commit

--- src/main/java/com/team/springboot/myspringboot/config/AliyunOssConfig.java
+++ src/main/java/com/team/springboot/myspringboot/config/AliyunOssConfig.java
@@ -1,11 +1,8 @@
 package com.team.springboot.myspringboot.config;
 
-import com.aliyun.oss.OSSClient;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.stereotype.Component;
 
 
